69265c5cfa229027c603fbe385df1693d9ebe6fa
BadPetrovich <ivantusx@gmail.com>
2023-05-01 18:49:19 +0300

    Started to change App to MVC

---
+++ src/main/java/mvc/SpringBootMyApplication.java
@@ -0,0 +1,12 @@
+package mvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootMyApplication.class, args);
+    }
+}
---
+++ src/main/webapp/WEB-INF/view/first-view.jsp
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+
+<html>
+<body>
+
+<h2>I wish you HNY!</h2>
+
+</body>
+
+</html>
