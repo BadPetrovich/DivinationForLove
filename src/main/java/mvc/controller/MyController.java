2ef6116efa73d16a693348100524987105e9db7d
BadPetrovich <ivantusx@gmail.com>
2023-05-13 01:33:21 +0300

    Trying to resolve no mapping for first page

--- src/main/java/mvc/controller/MyController.java
+++ src/main/java/mvc/controller/MyController.java
@@ -2,11 +2,12 @@ package mvc.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class MyController {
 
-    @RequestMapping("/")
+    @RequestMapping(value = "/", method = RequestMethod.GET)
     public String showFirstView(){
         return "first-view";
     }
